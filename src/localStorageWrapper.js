25401adb5b92fea2b58d885ff724a6ab447691d2
Moez Bouhlel <bmoez.j@gmail.com>
2014-01-10 03:25:09 +0100

    set loclStorage to undefined when the code is loaded inside a google chrome packaged webapp so it does not throw an exception

--- src/localStorageWrapper.js
+++ src/localStorageWrapper.js
@@ -2,8 +2,10 @@
 
     'use strict';
 
-    // Initialize localStorage and create a variable to use throughout the code.
-    var localStorage = window.localStorage;
+    // Initialize localStorage and create a variable to use throughout the code as long it's not running inside a google chrome packaged webapp.
+    var localStorage = undefined;
+    if (typeof(chrome) === "undefined" || typeof(chrome.runtime) === "undefined")
+      localStorage = window.localStorage;
 
     
     // If IndexedDB isn't available, we'll fall back to localStorage.
