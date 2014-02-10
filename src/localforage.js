037002073248bde32b8c327466dbf978299efa10
Moez Bouhlel <bmoez.j@gmail.com>
2014-02-10 21:05:34 +0100

    fix localForage export

--- src/localforage.js
+++ src/localforage.js
@@ -112,7 +112,7 @@
             return localForage;
         });
     } else if (moduleType === MODULE_TYPE_EXPORT) {
-        module.exports = localforage;
+        module.exports = localForage;
     } else {
         this.localforage = localForage;
     }
