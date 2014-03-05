492c913016139e9c22f2e7eff5f0de0e65436f4a
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-03-05 16:51:13 -0500

    Cleanup spaces and style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,51 @@
-module.exports = exports = function (grunt) {
-  'use strict';
-  grunt.initConfig({
-    concat: {
-      options: {
-        separator: '',
-      },
-      localforage: {
-        src: ['vendor/promise.js', 'src/drivers/**/*.js', 'src/localforage.js'],
-        dest: 'dist/localforage.js',
-        options: {
-          banner:
-            '/*!\n' +
-            '  localForage -- Offline Storage, Improved\n' +
-            '  http://mozilla.github.io/localForage\n' +
-            '  (c) 2013-2014 Mozilla, Apache License 2.0\n' +
-            '*/\n'
+/*global exports:true, module:true */
+module.exports = exports = function(grunt) {
+    'use strict';
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: '',
+            },
+            localforage: {
+                src: [
+                    'vendor/promise.js',
+                    'src/drivers/**/*.js',
+                    'src/localforage.js'
+                ],
+                dest: 'dist/localforage.js',
+                options: {
+                    banner:
+                        '/*!\n' +
+                        '    localForage -- Offline Storage, Improved\n' +
+                        '    http://mozilla.github.io/localForage\n' +
+                        '    (c) 2013-2014 Mozilla, Apache License 2.0\n' +
+                        '*/\n'
+                }
+            },
+            backbone: {
+                src: [
+                    'src/adapters/backbone.js'
+                ],
+                dest: 'dist/backbone.localforage.js',
+            }
+        },
+        uglify: {
+            localforage: {
+                files: {
+                    'dist/localforage.min.js': ['dist/localforage.js']
+                }
+            },
+            backbone: {
+                files: {
+                    'dist/backbone.localforage.min.js': ['dist/backbone.localforage.js']
+                }
+            }
         }
-      },
-      backbone: {
-        src: ['src/adapters/backbone.js'],
-        dest: 'dist/backbone.localforage.js',
-      }
-    },
-    uglify: {
-      localforage: {
-        files: {
-          'dist/localforage.min.js': ['dist/localforage.js']
-        }
-      },
-      backbone: {
-        files: {
-          'dist/backbone.localforage.min.js': ['dist/backbone.localforage.js']
-        }
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['concat', 'uglify']);
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['concat', 'uglify']);
 };
--- dist/localforage.js
+++ dist/localforage.js
@@ -1,7 +1,7 @@
 /*!
-  localForage -- Offline Storage, Improved
-  http://mozilla.github.io/localForage
-  (c) 2013-2014 Mozilla, Apache License 2.0
+    localForage -- Offline Storage, Improved
+    http://mozilla.github.io/localForage
+    (c) 2013-2014 Mozilla, Apache License 2.0
 */
 // ES6 Promises Polyfill
 // Version 0.1.1
