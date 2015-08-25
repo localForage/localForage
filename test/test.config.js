a716672abe44c138a65c473b187685b6e0e0c6e4
Thodoris Greasidis <thgreasi@gmail.com>
2015-08-25 21:41:21 +0300

    test(config): fix race condition in driver configuration test

--- test/test.config.js
+++ test/test.config.js
@@ -27,13 +27,16 @@ describe('Config API', function() {
         localforage.config(this.defaultConfig);
     });
 
-    it('returns the default values', function() {
+    it('returns the default values', function(done) {
         expect(localforage.config('description')).to.be('');
-        expect(localforage.config('driver')).to.be(localforage.driver());
         expect(localforage.config('name')).to.be('localforage');
         expect(localforage.config('size')).to.be(4980736);
         expect(localforage.config('storeName')).to.be('keyvaluepairs');
         expect(localforage.config('version')).to.be(1.0);
+        localforage.ready(function() {
+            expect(localforage.config('driver')).to.be(localforage.driver());
+            done();
+        });
     });
 
     it('returns error if API call was already made', function(done) {
