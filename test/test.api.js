28139d63d39f725e1e2bdc657cb9478070e8d0cd
afil <af7@users.noreply.github.com>
2015-08-25 12:15:30 +0300

    Fix the iterationNumber logic on localStorage

--- test/test.api.js
+++ test/test.api.js
@@ -262,6 +262,7 @@ DRIVERS.forEach(function(driverName) {
         });
 
         it('should iterate() through only its own keys/values', function(done) {
+
             localStorage.setItem('local', 'forage');
             localforage.setItem('office', 'Initech').then(function() {
                 return localforage.setItem('name', 'Bob');
@@ -269,15 +270,21 @@ DRIVERS.forEach(function(driverName) {
                 // Loop through all key/value pairs; {local: 'forage'} set
                 // manually should not be returned.
                 var numberOfItems = 0;
-                localforage.iterate(function(value, key) {
+                var iterationNumberConcat = '';
+                localforage.iterate(function(value, key, iterationNumber) {
                     expect(key).to.not.be('local');
                     expect(value).to.not.be('forage');
                     numberOfItems++;
+                    iterationNumberConcat += iterationNumber;
                 }, function(err) {
                     if (!err) {
                         // While there are 3 items in localStorage,
-                        // only two items were set using localForage.
+                        // only 2 items were set using localForage.
                         expect(numberOfItems).to.be(2);
+
+                        // Only 2 items were set using localForage,
+                        // so we should get '12' and not '123'
+                        expect(iterationNumberConcat).to.be('12');
 
                         done();
                     }
