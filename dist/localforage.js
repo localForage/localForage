da84e056c9097bfe452fadfd34bd2522588a51e6
Matthew Riley MacPherson <matt@lonelyvegan.com>
2015-08-29 15:40:05 +0200

    Fix browserify/webpack issues. 1.2.10

--- dist/localforage.js
+++ dist/localforage.js
@@ -1,6 +1,6 @@
 /*!
     localForage -- Offline Storage, Improved
-    Version 1.2.9
+    Version 1.2.10
     https://mozilla.github.io/localForage
     (c) 2013-2015 Mozilla, Apache License 2.0
 */
