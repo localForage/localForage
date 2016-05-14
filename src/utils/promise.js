b20959468565c6c99f8e544892f50e9bfd9c1dfe
Thodoris Greasidis <thgreasi@gmail.com>
2016-05-14 17:18:33 +0300

    fix(promise.js): do not import lie when unnecessary

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -1,4 +1,6 @@
 // This is CommonJS because lie is an external dependency, so Rollup
 // can just ignore it.
-require('lie/polyfill');
+if (typeof Promise === 'undefined' && typeof require !== 'undefined') {
+    require('lie/polyfill');
+}
 export default Promise;
