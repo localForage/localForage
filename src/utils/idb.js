e3f22188309060755627c584c0c48bc4348689c0
Thodoris Greasidis <thgreasi@gmail.com>
2016-07-24 18:05:15 +0300

    fix(utils/idb): fix SecurityError on FF (#595)
    Resolves #501.
    Based on the suggestion of @daviddeutsch.

--- src/utils/idb.js
+++ src/utils/idb.js
@@ -1,20 +1,22 @@
 function getIDB() {
     /* global indexedDB,webkitIndexedDB,mozIndexedDB,OIndexedDB,msIndexedDB */
-    if (typeof indexedDB !== 'undefined') {
-        return indexedDB;
-    }
-    if (typeof webkitIndexedDB !== 'undefined') {
-        return webkitIndexedDB;
-    }
-    if (typeof mozIndexedDB !== 'undefined') {
-        return mozIndexedDB;
-    }
-    if (typeof OIndexedDB !== 'undefined') {
-        return OIndexedDB;
-    }
-    if (typeof msIndexedDB !== 'undefined') {
-        return msIndexedDB;
-    }
+    try {
+        if (typeof indexedDB !== 'undefined') {
+            return indexedDB;
+        }
+        if (typeof webkitIndexedDB !== 'undefined') {
+            return webkitIndexedDB;
+        }
+        if (typeof mozIndexedDB !== 'undefined') {
+            return mozIndexedDB;
+        }
+        if (typeof OIndexedDB !== 'undefined') {
+            return OIndexedDB;
+        }
+        if (typeof msIndexedDB !== 'undefined') {
+            return msIndexedDB;
+        }
+    } catch (e) { }
 }
 
 var idb = getIDB();
