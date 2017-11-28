3e17088f07f0c88964c750ecb3180085fdedc04c
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2017-11-28 18:40:48 +0000

    chore: Release version 1.5.4

--- dist/localforage.js
+++ dist/localforage.js
@@ -1,6 +1,6 @@
 /*!
     localForage -- Offline Storage, Improved
-    Version 1.5.3
+    Version 1.5.4
     https://localforage.github.io/localForage
     (c) 2013-2017 Mozilla, Apache License 2.0
 */
