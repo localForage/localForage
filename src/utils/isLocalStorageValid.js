aea1913c9f078463781f81bbe29a7dd650f1c356
Thodoris Greasidis <thgreasi@gmail.com>
2018-02-03 23:03:33 +0200

    fix localStorage in IE8
    This is a regression from #723
    See: https://github.com/localForage/localForage/pull/723/files#diff-c308623615f5f6f9fe5127ca5e673668

--- src/utils/isLocalStorageValid.js
+++ src/utils/isLocalStorageValid.js
@@ -1,8 +1,11 @@
 function isLocalStorageValid() {
     try {
-        return typeof localStorage !== 'undefined' &&
-            ('setItem' in localStorage) &&
-            typeof localStorage.setItem === 'function';
+        return (
+            typeof localStorage !== 'undefined' &&
+            'setItem' in localStorage &&
+            // in IE8 typeof localStorage.setItem === 'object'
+            !!localStorage.setItem
+        );
     } catch (e) {
         return false;
     }
