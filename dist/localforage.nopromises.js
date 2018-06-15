e131383a23763d087634619fa6cd63375a285c47
Thodoris Greasidis <thgreasi@gmail.com>
2018-06-15 09:43:17 +0300

    dist: fix websql dropped db detection

--- dist/localforage.nopromises.js
+++ dist/localforage.nopromises.js
@@ -1373,7 +1373,7 @@ function _initStorage$1(options) {
 function tryExecuteSql(t, dbInfo, sqlStatement, args, callback, errorCallback) {
     t.executeSql(sqlStatement, args, callback, function (t, error) {
         if (error.code === error.SYNTAX_ERR) {
-            t.executeSql('SELECT name FROM sqlite_master ' + "WHERE type='table' AND name = ?", [name], function (t, results) {
+            t.executeSql('SELECT name FROM sqlite_master ' + "WHERE type='table' AND name = ?", [dbInfo.storeName], function (t, results) {
                 if (!results.rows.length) {
                     // if the table is missing (was deleted)
                     // re-create it table and retry
