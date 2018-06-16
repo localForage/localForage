7428cfc4a6fd60ac00346619b923bfb8e17e399a
Thodoris Greasidis <thgreasi@gmail.com>
2018-06-16 20:36:10 +0300

    chore: Release version 1.7.2

--- dist/localforage.js
+++ dist/localforage.js
@@ -1,6 +1,6 @@
 /*!
     localForage -- Offline Storage, Improved
-    Version 1.7.1
+    Version 1.7.2
     https://localforage.github.io/localForage
     (c) 2013-2017 Mozilla, Apache License 2.0
 */
