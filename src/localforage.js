6622e72000eda6f12eb58c91195c530eecfdfc4f
Himanshu Joshi <himanshu@commutec.in>
2020-05-03 19:24:24 +0530

    end .

--- src/localforage.js
+++ src/localforage.js
@@ -43,7 +43,7 @@ const DefaultConfig = {
     description: '',
     driver: DefaultDriverOrder.slice(),
     name: 'localforage',
-    // Default DB size is _JUST UNDER_ 5MB, as it's the highest size
+    // Default DB size is _JUST UNDER_ 5MB, as it's the highest size.
     // we can use without a prompt.
     size: 4980736,
     storeName: 'keyvaluepairs',
